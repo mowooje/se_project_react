2e76efe7b815301d4aea391bfe3c0951a581a62b
Michael Owooje <michaelowooje@gmail.com>
2024-10-21 15:37:39 -0400

    Header styling complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,17 @@ import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.svg";
 
-function Header() {
+function Header(weatherData) {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
   return (
     <header className="header">
       <img className="header__logo" src={logo} />
-      <p className="header__date-and-location">DATE, LOCATION</p>
+      <p className="header__date-and-location">
+        {currentDate}, {weatherData.city}
+      </p>
       <button className="header__add-clothes-btn">+ Add Clothes</button>
       <div className="header__user-container">
         <p className="header__username">Terrence Tegegne</p>
