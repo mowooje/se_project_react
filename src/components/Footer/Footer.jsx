466b9b945a7a67c76b13c7e51f1ef5e03d5bde28
Michael Owooje <michaelowooje@gmail.com>
2024-10-27 22:21:29 -0400

    fix footer className error

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div class="footer__name">Developed by Michael Owooje</div>
-      <div class="footer__year">2024</div>
+      <div className="footer__name">Developed by Michael Owooje</div>
+      <div className="footer__year">2024</div>
     </footer>
   );
 };
