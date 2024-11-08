f4635149c47726d05af403c7b91e371b3a90a4c5
Michael Owooje <michaelowooje@gmail.com>
2024-11-08 18:35:24 -0500

    can't fix weatherData console error

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,35 +4,38 @@ import ItemCard from "../ItemCard/ItemCard";
 import { defaultClothingItems } from "../../utils/constants";
 import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext";
 import { useContext } from "react";
+import { BrowserRouter } from "react-router-dom";
 
-function Main({ weatherData, handleCardClick }) {
+function Main({ weatherData = {}, handleCardClick }) {
   const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
   const temp = weatherData?.temp?.[currentTemperatureUnit] || 999;
 
   return (
-    <main>
-      <WeatherCard weatherData={weatherData} />
-      <section className="cards">
-        <p className="cards__text">
-          Today is {temp} {currentTemperatureUnit} / You may want to wear:
-        </p>
-        <ul className="cards__list">
-          {defaultClothingItems
-            .filter((item) => {
-              return item.weather === weatherData.type;
-            })
-            .map((item) => {
-              return (
-                <ItemCard
-                  key={item._id}
-                  item={item}
-                  onCardClick={handleCardClick}
-                />
-              );
-            })}
-        </ul>
-      </section>
-    </main>
+    <BrowserRouter>
+      <main>
+        <WeatherCard weatherData={weatherData} />
+        <section className="cards">
+          <p className="cards__text">
+            Today is {temp} {currentTemperatureUnit} / You may want to wear:
+          </p>
+          <ul className="cards__list">
+            {defaultClothingItems
+              .filter((item) => {
+                return item.weather === weatherData.type;
+              })
+              .map((item) => {
+                return (
+                  <ItemCard
+                    key={item._id}
+                    item={item}
+                    onCardClick={handleCardClick}
+                  />
+                );
+              })}
+          </ul>
+        </section>
+      </main>
+    </BrowserRouter>
   );
 }
 
